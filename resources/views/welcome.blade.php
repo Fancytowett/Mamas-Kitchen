13f8860285373b62045843acf4854c467d85b123
Fancy <fancytowett@gmail.com>
2018-11-14 09:02:02 +0300

    bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,7 @@
     <style type="text/css">
         @foreach($sliders as $key=>$slider)
         .owl-carousel .owl-wrapper, .owl-carousel .owl-item:nth-child({{$key+1}}).item {
-            background:url({{asset('uploads/slider/'.$slider->image)}});
+            background-image:url({{asset('uploads/slider/'.$slider->image)}});
             background-size:cover;
         }
         @endforeach
